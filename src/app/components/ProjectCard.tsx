2e99d4126b3214b63a2c88a3662d18ac6bd58592
Rohan Venkatesha <149955518+rohanvenkatesha@users.noreply.github.com>
2025-08-02 20:59:09 -0400

    Update ProjectCard.tsx

--- src/app/components/ProjectCard.tsx
+++ src/app/components/ProjectCard.tsx
@@ -2,7 +2,6 @@
 
 import { useState } from 'react';
 import { motion, AnimatePresence } from 'framer-motion';
-import { ArrowUpRight } from 'lucide-react';
 import { type Project } from '@/lib/projects';
 
 // --- COLOR LOGIC ---
