78b7b443441cc2fccbe961662b179fa9780ff1c0
Rohan Venkatesha <149955518+rohanvenkatesha@users.noreply.github.com>
2025-08-06 19:16:45 -0400

    Update RideMap.tsx

--- src/app/components/ride/RideMap.tsx
+++ src/app/components/ride/RideMap.tsx
@@ -6,6 +6,7 @@ import 'leaflet/dist/leaflet.css';
 import { useEffect } from 'react';
 import { useMap } from 'react-leaflet/hooks';
 
+// @ts-ignore
 delete (L.Icon.Default.prototype as any)._getIconUrl;
 
 L.Icon.Default.mergeOptions({
