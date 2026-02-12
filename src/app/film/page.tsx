53864f6ec72d7281f5eccf77863d4516c718d10b
Rohan Venkatesha <149955518+rohanvenkatesha@users.noreply.github.com>
2026-02-12 13:36:24 -0500

    Update page.tsx

--- src/app/film/page.tsx
+++ src/app/film/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import { motion, AnimatePresence, useScroll, useTransform } from 'framer-motion';
 import { 
   Camera, 
