e1aa532b7c05c07ad77b8cd7e5212c717cdc881d
Rohan Venkatesha <149955518+rohanvenkatesha@users.noreply.github.com>
2026-02-23 15:09:51 -0500

    Update FilmFooter.tsx

--- src/app/components/FilmFooter.tsx
+++ src/app/components/FilmFooter.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import React, { useState, useEffect, useRef } from 'react';
+import React, { useState, useRef } from 'react';
 import { motion, useScroll, useTransform, useSpring } from 'framer-motion';
 import { 
   Github, 
